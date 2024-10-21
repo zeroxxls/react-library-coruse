e9ba0da3cb1e20e70da754c74e80f7ba2899de78
zeroxxl <zeroxxhls@gmail.com>
2024-10-21 23:29:29 +0200

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import List from "./components/List";
 import * as styles from "./App.module.css";
 
 const App = () =>{
+   const [counter, setCounter] = useState(0);
+
+   const handlePlusBtnClick = (event)=>{
+    console.log(event)
+    setCounter(counter + 1)
+   }
+
     return <List text ="text">
-    <div className={styles.test}>1</div>
-    <div className={styles.test}>2</div>
-    <div className={styles.test}>3</div>
+    <div className={styles.test}>{counter}</div>
+    {counter %2 === 0 ? <p>Четное</p> : <p>Не Четное</p>}
+    <button onClick={handlePlusBtnClick}>plus</button>
     </List>
 };
 
